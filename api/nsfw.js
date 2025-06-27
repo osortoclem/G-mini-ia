d0a556daa49c37e0ee219d9bfa34eeb054a4f685
Deylin <deylinbo91@gmail.com>
2025-06-26 18:45:18 -0600

    Actualizar nsfw.js

--- api/nsfw.js
+++ api/nsfw.js
@@ -11,13 +11,15 @@ export default async function handler(req, res) {
     const data = await response.json();
 
     res.status(200).json({
+      autor: 'Deylin',
       type,
       url: data.url
     });
   } catch (error) {
     res.status(500).json({
       error: 'Error al obtener imagen NSFW',
-      detalle: error.message
+      detalle: error.message,
+      autor: 'Deylin'
     });
   }
 }
