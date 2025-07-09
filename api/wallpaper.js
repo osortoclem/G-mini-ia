c7d0ab5a752ea57e833e28a06dbfd56eacf7e62e
Deylin <deylinbo91@gmail.com>
2025-07-08 22:27:48 -0600

    Actualizar wallpaper.js

--- api/wallpaper.js
+++ api/wallpaper.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import cheerio from 'cheerio'
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   const { q } = req.query
   if (!q) return res.status(400).json({ error: 'Falta el parámetro ?q=' })
 
